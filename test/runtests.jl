87fc8ef0e77d814fcf18740aa0f6db319383de13
AxLamelas <alamelas@ua.pt>
2022-04-28 17:23:29 +0100

    Started adding simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,15 @@
+using VLEvolution
 using Test
 
-@test true
+@testset "Structure" begin
+    p = Param(0.,-1,1)
+    @test p.val == 0.
+    @test p.lb == -1.
+    @test p.ub == 1.
+    @test typeof(p.lb) <: AbstractFloat
+
+end
+
+@testset "Fit" begin
+    
+end
