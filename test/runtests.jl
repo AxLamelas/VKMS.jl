95f11b69719104025b710e034787f1e3d9a2edd2
AxLamelas <alamelas@ua.pt>
2022-04-28 17:44:50 +0100

    Fittings tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,5 +11,20 @@ using Test
 end
 
 @testset "Fit" begin
+    state = OptimParameters(64, 5.5, 0.05, 0.05, 50.0, 0.1, 2, :less, 2)
+    functional(x::AbstractVector,f) = @. 2f(x) + x + 1
+    functional(x::AbstractVector,m::KnotModel) = functional(x,model_function_factory(m)) .+ m.m.val .+ m.b.val
+    target_function = sin
+    x = -10:0.2:10
+    y = functional(x,target_function)
+
+    fitness = fitness_factory(functional,x,y)
+
+    pop = random_population(10,(minimum(x),maximum(x)),(-1.5,1.5),state.pop_size,m -> fitness(state,m)[1])
+    @test all(get_n_metavariables.(pop) .== 10)
+
+    final_pop = evolve(pop, fitness, state, max_gen = 100, info_every=nothing)
     
+    @test typeof(pop) == typeof(final_pop)
+    @test length(pop) == length(final_pop) == state.pop_size
 end
